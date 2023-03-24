0929f0ba87ea0e70cf4cd508912f8f8d93fd4b8f
Tetiana <121768305+Tanyuwik@users.noreply.github.com>
2023-03-24 21:18:39 +0100

    added forecast in js

--- app.js
+++ app.js
@@ -21,6 +21,36 @@ let day = days[now.getDay()];
 let today = document.querySelector("#date");
 today.innerHTML = `${day} ${hours}:${minutes}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu"];
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML +
+    `
+          <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img 
+            src="http://openweathermap.org/img/wn/50d@2x.png" 
+            alt="" 
+            width="42"
+            />
+            <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperatures-max"> 
+              18° 
+            </span>
+            <span class="weather-forecast-temperatures-min">
+              12°
+            </span>
+          </div>
+          </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function unitCelsius(event) {
   event.preventDefault();
   celsius.classList.add("active");
@@ -99,3 +129,4 @@ let button = document.querySelector("#current");
 button.addEventListener("click", showGeoLocation);
 
 search("Barcelona");
+displayForecast();
